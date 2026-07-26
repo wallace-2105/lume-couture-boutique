1bb919e58c37e5b35b789a99dc150b1f8070acc2
wallace-2105 <wallace.wcs83@gmail.com>
2026-07-25 21:27:41 -0300

    feat: replace Lovable favicon with LUME brand SVG icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -87,7 +87,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+      { rel: "alternate icon", href: "/favicon.svg" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
