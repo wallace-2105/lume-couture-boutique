537b9bca6e46390fb6b32adcf1bbe1b013d8e1a8
wallace-2105 <wallace.wcs83@gmail.com>
2026-08-03 16:39:17 -0300

    fix: alterar limite de parcelamento de 10x para 3x

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,7 +1,7 @@
 export const formatPrice = (value: number) =>
   value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 
-export const installments = (value: number, times = 10) => ({
+export const installments = (value: number, times = 3) => ({
   times,
   value: value / times,
 });
