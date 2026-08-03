77f500f7eec625f95d1c108156f643dd2f9fc5fe
wallace-2105 <wallace.wcs83@gmail.com>
2026-08-03 16:29:35 -0300

    fix: alterar número máximo de parcelas de 12x para 10x

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,7 +1,7 @@
 export const formatPrice = (value: number) =>
   value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 
-export const installments = (value: number, times = 12) => ({
+export const installments = (value: number, times = 10) => ({
   times,
   value: value / times,
 });
